c1399fa9892ce2da109d3aab9238feda72b65717
Emiliano Rodriguez <emrdz56@gmail.com>
2022-02-19 23:28:14 -0600

    nuevos cambios

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ console.log("Hola mundo")
 var total=0;
 var monto=0;
 var totalMsg=document.getElementById("totalMsg");
-
+var descripcionTxt = document.getElementById("descripcion");
+var listaTicket = document.getElementById("ticket");
 var montoTxt=document.getElementById("montoTxt");
 //console.log("montoTxt");
 
@@ -10,6 +11,18 @@ function calcularTotal(){
     monto=montoTxt.value;
     monto=parseInt(monto);
     total=total+monto;
-    console.log(total);
+    //console.log(total);
     totalMsg.innerHTML="Total:" + total;
+    imprimirTicket(monto);
+}
+
+function imprimirTicket(monto){
+    var elementoTicket = document.createElement("ion-item");
+    var labelElemento = document.createElement("ion-label");
+
+    labelElemento.innerHTML = descripcionTxt.value+" : $"+monto;
+    elementoTicket.appendChild(labelElemento);
+    listaTicket.appendChild(elementoTicket);
+    descripcionTxt.value= "";
+    montoTxt.value="";
 }
